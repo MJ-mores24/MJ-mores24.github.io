067d4ff01c3796bfe02b1b44f3a46f2bbab799d3
MJ <marvinmoresca@gmail.com>
2025-01-30 16:58:46 +0800

    first commit

---
+++ PHPMailer.php
@@ -0,0 +1,39 @@
+<?php
+session_start();
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+if(isset($_POST['form_submit'])){
+  $form_name = $_POST['form_name'];
+  $form_email = $_POST['form_email'];
+  $form_subject = $_POST['form_subject'];
+  $form_message = $_POST['form_message'];
+  $mail = new PHPMailer(true);
+  try {
+    $mail->isSMTP();
+    $mail->Host       = 'mail.fcgroup.ph';
+    $mail->SMTPAuth   = true;
+    $mail->Username   = 'it_bpi@fcgroup.ph';
+    $mail->Password   = 'floorcenter';
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+    $mail->Port       = 465;
+    $mail->setFrom($form_email, $form_name);
+    $mail->addAddress('marvinmoresca@gmail.com', '');
+    $mail->isHTML(true);
+    $mail->Subject = $form_subject;
+    $mail->Body    = '';
+    $mail->AltBody = '';
+    $mail->send();
+    $_SESSION['icon'] = "success";
+    $_SESSION['message'] = "Your message was sent.";
+    header('Location: ' . $_SERVER['HTTP_REFERER']);
+  } catch (Exception $e) {
+    $_SESSION['icon'] = "error";
+    $_SESSION['message'] = "Error in sending message.";
+    header('Location: ' . $_SERVER['HTTP_REFERER']);
+  }
+}
+?>
